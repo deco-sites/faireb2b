15a1f821c61f5fe6cda553b91a61423985af5deb
Marco Ferreira <mw.marcowilliam@gmail.com>
2023-06-25 19:59:47 -0300

    feat: setup inicial

--- components/GlobalTags.tsx
+++ components/GlobalTags.tsx
@@ -11,6 +11,66 @@ function GlobalTags() {
 
       {/* Web Manifest */}
       <link rel="manifest" href={asset("/site.webmanifest")} />
+
+      <style
+        dangerouslySetInnerHTML={{
+          __html: `
+          /* latin */
+          @font-face {
+            font-family: 'Graphik';
+            font-style: normal;
+            font-weight: 400;
+            font-display: swap;
+            src: url(${
+            asset("/fonts/Graphik-Regular-Web.woff2")
+          }) format('woff2');
+            unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+          }
+          /* latin-ext */
+          @font-face {
+            font-family: 'Graphik';
+            font-style: normal;
+            font-weight: 500;
+            font-display: swap;
+            src: url(${
+            asset("/fonts/Graphik-Medium-Web.woff2")
+          }) format('woff2');
+            unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+          }
+          /* latin-ext */
+          @font-face {
+            font-family: 'Graphik';
+            font-style: normal;
+            font-weight: 600;
+            font-display: swap;
+            src: url(${
+            asset("/fonts/Graphik-Semibold-Web.woff2")
+          }) format('woff2');
+            unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+          }
+
+          /* latin-ext */
+          @font-face {
+            font-family: 'nantes';
+            font-style: normal;
+            font-weight: 400;
+            font-display: swap;
+            src: url(${asset("/fonts/NantesWeb-Regular")}) format('woff2');
+            unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+          }
+
+          /* latin-ext */
+          @font-face {
+            font-family: 'nantes';
+            font-style: normal;
+            font-weight: 600;
+            font-display: swap;
+            src: url(${asset("/fonts/NantesWeb-Book.woff2")}) format('woff2');
+            unicode-range: U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF;
+          }
+          `,
+        }}
+      />
     </Head>
   );
 }
